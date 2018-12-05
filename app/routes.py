f10102ba9f0417bf17103a88d11c95108da9c0cc
Adam Ingwersen <adamlinnemann@gmail.com>
2018-12-05 12:49:52 +0900

    Add about me interface, route and model

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app, db
 from app.models import User
 from datetime import datetime
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_user, logout_user, login_required
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
@@ -66,3 +66,18 @@ def user(username):
         {'author': user, 'body': 'My second post'}
     ]
     return(render_template('user.html', user = user, posts = posts))
+
+@app.route('/edit_profile', methods = ['POST', 'GET'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your profile description has been updated!')
+        return(redirect(url_for('edit_profile')))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return(render_template('edit_profile.html', title = 'Edit Profile', form = form))
