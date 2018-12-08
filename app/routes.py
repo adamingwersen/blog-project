6cd64627ba46ef88df055745d04ac173698dfcbc
Adam Ingwersen <adamlinnemann@gmail.com>
2018-12-08 14:53:56 +0900

    Create browse redirect to show only posts. Expand _post template. Extend base.html.

--- app/routes.py
+++ app/routes.py
@@ -51,13 +51,13 @@ def index():
 """
  - BOARD -
 """
-@app.route('/board', methods = ['GET', 'POST'])
-def board():
+@app.route('/browse', methods = ['GET', 'POST'])
+def browse():
     page    = request.args.get('page', 1, type = int)
     posts   = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
-    next_url    = url_for('index', page = posts.next_num) if posts.has_next else None
-    prev_url    = url_for('index', page = posts.prev_num) if posts.has_prev else None
-    return(render_template('board.html',  title = "Board", posts = posts.items, next_url = next_url, prev_url = prev_url))
+    next_url    = url_for('browse', page = posts.next_num) if posts.has_next else None
+    prev_url    = url_for('browse', page = posts.prev_num) if posts.has_prev else None
+    return(render_template('browse.html',  title = "Browse", posts = posts.items, next_url = next_url, prev_url = prev_url))
 
 
 """
